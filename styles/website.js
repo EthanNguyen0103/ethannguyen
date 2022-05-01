dcf6e13ed83fe055791415785c268c057bd70702
Ethan Nguyen <58565070+EthanNguyen0103@users.noreply.github.com>
2022-05-02 01:14:41 +0700

    version--0.03
    add new features, bug fixs

--- styles/website.js
+++ styles/website.js
@@ -1,3 +1,29 @@
-setTimeout(() =>{
-  document.getElementById('mySound').play();
-}, 1000);
+const mutilStep = document.querySelector("[multi-step]");
+const fontSteps = [...mutilStep.querySelectorAll("[data-step]")]
+const mySound = document.getElementById('mySound');
+let currentStep = fontSteps.findIndex(step  => {
+  return step.classList.contains("active")
+})
+if (currentStep <0){
+  currentStep = 0
+  showCurrenStep()
+}
+
+mutilStep.addEventListener("click", e =>{
+  if(e.target.matches("[data-next]")){
+      currentStep +=1
+  }
+  showCurrenStep()
+})
+function showCurrenStep(){
+  fontSteps.forEach((step, index)  =>{
+    step.classList.toggle("active", index === currentStep)
+  })
+  setTimeout(() => {
+    const mySound = document.getElementById('mySound');
+    mySound.play()
+  }, 1000);
+}
+
+
+
